5543537b8a603756b1ae88224e85db25224777f7
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2018-02-24 16:01:31 +0000

    Git related tasks WIP

--- src/release-tasks.js
+++ src/release-tasks.js
@@ -1,10 +1,18 @@
-const through = require('through2');
 const exec = require('./gulp-exec');
+const runSequence = require('run-sequence');
+const escape = require('any-shell-escape');
+
+const shellCommands = {
+  checkout: 'git checkout master',
+  pull: 'git pull',
+  push: 'git push',
+  pushTags: 'git push --tags'
+};
 
 function create(gulp, options) {
   /*
-   * 1. Switch to master
-   * 2. Pull
+   * +1. Switch to master
+   * +2. Pull
    * 3. Bump
    * 4. Add + Commit
    * 5. Create tag
@@ -12,10 +20,45 @@ function create(gulp, options) {
    * 7. Push master
    */
 
-  gulp.task('imprint:release:git:prepare', async() => {
-    await exec('git checkout master');
-    await exec('git pull');
+  gulp.task('imprint:release:git:prepare', async () => {
+    if (options.dryRun) {
+      console.log(shellCommands.checkout);
+      console.log(shellCommands.pull);
+    } else {
+      await exec(shellCommands.checkout);
+      await exec(shellCommands.pull);
+    }
   });
+
+  gulp.task('imprint:release:git:tag', async () => {
+    const version = require(options.packageFile).version;
+    const versionMessage = options.commitPrefix + version;
+    const addCmd = `git add ${escape(options.packageFile)}`;
+    const commitCmd = `git commit -m "${versionMessage}"`;
+    const tagCmd = `git tag -a ${escape(version)} -m "${versionMessage}"`;
+
+    if (options.dryRun) {
+      console.log(addCmd);
+      console.log(commitCmd);
+      console.log(tagCmd);
+      console.log(shellCommands.push);
+      console.log(shellCommands.pushTags);
+    } else {
+      await exec(addCmd);
+      await exec(commitCmd);
+      await exec(tagCmd);
+      await exec(shellCommands.push);
+      await exec(shellCommands.pushTags);
+    }
+  });
+
+  gulp.task(
+    'imprint:release:patch',
+    done => runSequence(
+      'imprint:release:git:prepare',
+      'imprint:bump:patch',
+      'imprint:release:git:tag',
+      done))
 }
 
 module.exports = create;
